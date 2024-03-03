be390195272ca864899cdd905f1ff74a253aeea7
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-03-03 11:36:02 +0530

    INIT +1

--- app.py
+++ app.py
@@ -9,4 +9,12 @@ df = pd.read_csv("./data.csv")
 
 st.title(":red[__SvS__] Data 500 :sunglasses:", anchor="head", help="State 500 vs 497")
 
+df
 
+score = df.sum(1, True, True)[0]
+
+# st.write(score)
+
+st.metric("DMN final score", score)
+
+st.metric("DMN Day 1", df.)
