9632ca9655c372ff91c0e2460169a2cc44426ecf
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-03-11 09:57:02 +0530

    Creating the selector for state and alliance

--- app.py
+++ app.py
@@ -13,54 +13,21 @@ df = pd.read_csv("./data.csv")
 
 st.title(":red[__SvS__] Data 500 :sunglasses:", anchor="head", help="State 500 vs 497")
 
+# print(df)
 
-df
+st.session_state.state = "500"
+st.session_state.alliance = "DMN"
+st.session_state.disabled = False
 
-day_1_val=df['Day 1'].loc[df.index[0]]
-day_2_val=df['Day 2'].loc[df.index[0]]
-day_3_val=df['Day 3'].loc[df.index[0]]
-day_4_val=df['Day 4'].loc[df.index[0]]
-day_5_val=df['Day 5'].loc[df.index[0]]
-
-score = df.sum(1, True, True)[0]
-
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.metric("DMN final score", score)
+    st.checkbox("Enable comparator mode", key="disabled")
+    st.radio("Select State", key="state", options=["500", "497", "Both"])
 
 with col2:
-    st.metric("DMN Day 1 Score", day_1_val)
-
-with col3:
-    st.metric('DMN Day 2 Score', day_2_val)
-
-# col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.metric("DMN Day 3 score", day_3_val)
-
-with col2:
-    st.metric("DMN Day 4 Score", day_4_val)
-
-with col3:
-    st.metric('DMN Day 5 Score', day_5_val)
-
-with col1:
-    st.metric("DMN Day 1 Rank", 1)
-
-with col2:
-    st.metric("DMN Day 2 Rank", 1)
-
-with col3:
-    st.metric('DMN Day 3 Rank', 1)
-
-with col1:
-    st.metric("DMN Day 4 Rank", 1)
-
-with col2:
-    st.metric("DMN Day 5 Rank", 1)
-
-with col3:
-    st.metric('DMN Battle day rank', 2)
-
+    alliance = st.selectbox(
+        "Please Select an alliance to be compared to DMN",
+        df['Alliance'],
+        disabled=not st.session_state.disabled
+    )
