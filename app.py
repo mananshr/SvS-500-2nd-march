f83f9c471f9cc211257bba3e936dd53670f1df37
Manan Sharma <3783977+mananshr@users.noreply.github.com>
2024-03-14 23:34:30 +0530

    MVP

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.title(":red[__SvS__] Data 500 :sunglasses:", anchor="head", help="State 500 v
 if "state" not in st.session_state:
     st.session_state.state = 500
     st.session_state.disabled = False
-    st.session_state.alliance = 'DMN'
+    st.session_state.alliance = 'BOM'
 
 col1, col2 = st.columns(2)
 alliance_list = df["Alliance"]
@@ -31,43 +31,70 @@ with col1:
     )
 
 with col2:
-    alliance_list = df[df["State"]==st.session_state.state]
+    alliance_list = df[df["State"] == st.session_state.state]
     # print(alliance_list)
-    alliance_list=alliance_list["Alliance"]
+    alliance_list = alliance_list["Alliance"]
+    if st.session_state.state == 500:
+        alliance_list = alliance_list.drop(0)
     option = st.selectbox(
         "Compare Your Alliance with DMN",
         alliance_list,
+        # index=1,
         disabled=not st.session_state.disabled,
     )
     st.session_state.alliance = option
 
 if st.session_state.disabled:
+
     compared_state = dmn_data._append(df[df["Alliance"]==st.session_state.alliance])
     # compared_state
     st.area_chart(compared_state, x="Alliance", y=["Day 1", "Day 2","Day 3", "Day 4","Day 5"])
 
+    compared_state_temp = compared_state.rename(columns={'Day 6': 'Battle Day'})
+    compared_state_temp = compared_state_temp.drop("State", axis=1)
+    st.dataframe(compared_state_temp, hide_index=True)
+
+    compared_state_temp = compared_state_temp.drop("Battle Day", axis=1)
+    compared_state_temp = compared_state_temp.drop("Alliance", axis=1)
+    # sum_df = pd.DataFrame(columns=["Sum"])
+    compared_state_temp = compared_state_temp.sum(axis=1)
+    # compared_state_temp.columns = ["Sum"]
+    # compared_state_temp
+    sum_vals = pd.Series(compared_state_temp)
+    # sum_vals
+    sum_names = compared_state["Alliance"].squeeze()
+    # sum_names
+    sums_df = pd.concat([sum_names, sum_vals,], axis=1)
+    # sums_df.rename(columns={0: "Prep days total"})
+    sums_df.columns.values[1] = "Prep Days Total"
+    # sums_df
+
     st.subheader("Day 1")
-    fig = px.pie(compared_state, values="Day 1", names="Alliance")
+    fig = px.pie(compared_state, values="Day 1", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
     st.subheader("Day 2")
-    fig = px.pie(compared_state, values="Day 2", names="Alliance")
+    fig = px.pie(compared_state, values="Day 2", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
     st.subheader("Day 3")
-    fig = px.pie(compared_state, values="Day 3", names="Alliance")
+    fig = px.pie(compared_state, values="Day 3", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
     st.subheader("Day 4")
-    fig = px.pie(compared_state, values="Day 4", names="Alliance")
+    fig = px.pie(compared_state, values="Day 4", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
     st.subheader("Day 5")
-    fig = px.pie(compared_state, values="Day 5", names="Alliance")
+    fig = px.pie(compared_state, values="Day 5", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
+    st.plotly_chart(fig)
+
+    st.header("Prep Days Total Score")
+    fig = px.pie(sums_df, values='Prep Days Total', names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
     st.header("Battle Day")
-    fig = px.pie(compared_state, values="Day 6", names="Alliance")
+    fig = px.pie(compared_state, values="Day 6", names="Alliance", color_discrete_sequence=px.colors.sequential.RdBu)
     st.plotly_chart(fig)
 
 else:
